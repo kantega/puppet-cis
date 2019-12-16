af05081d143dd07b4a19c34f681b3ae6a87b2fe2
Geir <geirtufte@protonmail.com>
2019-12-16 07:44:08 +0100

    sshd loglevel benchmark

--- manifests/cis_5_2_5.pp
+++ manifests/cis_5_2_5.pp
@@ -1,29 +1,36 @@
-# 5.2.5 Ensure SSH MaxAuthTries is set to 4 or less (Scored)
+# 5.2.5 Ensure SSH LogLevel is appropriate (Scored)
 #
 # Description:
-# The MaxAuthTries parameter specifies the maximum number of authentication attempts permitted per connection. When the login failure count
-# reaches half the number, error messages will be written to the syslog file detailing the login failure.
+#
+# INFO level is the basic level that only records login activity of SSH users. In many situations,
+# such as Incident Response, it is important to determine when a particular user was active
+# on a system. The logout record can eliminate those users who disconnected, which helps
+# narrow the field.
+# VERBOSE level specifies that login and logout activity as well as the key fingerprint for any
+# SSH key used for login will be logged. This information is important for SSH key
+# management, especially in legacy environments.
 #
 # Rationale:
-# Setting the MaxAuthTries parameter to a low number will minimize the risk of successful brute force attacks to the SSH server. While the
-# recommended setting is 4, set the number based on site policy.
 #
-# @summary 5.2.5 Ensure SSH MaxAuthTries is set to 4 or less (Scored)
+# SSH provides several logging levels with varying amounts of verbosity. DEBUG is specifically
+# not recommended other than strictly for debugging SSH communications since it provides
+# so much data that it is difficult to identify important security information.
+#
+# @summary 5.2.5 Ensure SSH LogLevel is appropriate (Scored)
 #
 # @example
 #   include cis::5_2_5
 class cis::cis_5_2_5 (
   Boolean $enforced = true,
-  Enum['1', '2', '3', '4'] $max_auth_tries = '4',
+  Enum['INFO', 'VERBOSE'] $loglevel = 'INFO',
 ) {
 
   if $enforced {
-
-    file_line { 'ssh max auth tries':
+    file_line{ 'sshd loglevel setting':
       ensure => present,
       path   => '/etc/ssh/sshd_config',
-      line   => "MaxAuthTries ${max_auth_tries}",
-      match  => '^#?MaxAuthTries',
+      line   => "LogLevel ${loglevel}",
+      match  => '^LogLevel',
       notify => Service['sshd'],
     }
   }
