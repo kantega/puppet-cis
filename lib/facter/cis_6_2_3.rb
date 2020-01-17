8ab52ad954c46d494ecefc0d56cde0bac47f5a26
Geir Andre Tufte <Geir.Tufte@kantega.no>
2020-01-17 13:06:25 +0100

    implemented benchmark 6.2.3

--- lib/facter/cis_6_2_3.rb
+++ lib/facter/cis_6_2_3.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-# cis_6_2_4.rb
+# cis_6_2_3.rb
 
-Facter.add('cis_6_2_4') do
-  setcode "grep '^\\+:' /etc/shadow"
+Facter.add('cis_6_2_3') do
+  setcode do
+    Facter::Core::Execution.exec('/usr/local/bin/cis_6_2_3.sh')
+  end
 end
--- manifests/cis_6_2_3.pp
+++ manifests/cis_6_2_3.pp
@@ -1,1 +1,34 @@
-# TODO
+# 6.2.3 Ensure root PATH Integrity (Scored)
+
+#  Description:
+#
+#  The root user can execute any command on the system and could be fooled into executing
+#  programs unintentionally if the PATH is not set correctly.
+#
+#  Rationale:
+#
+#  Including the current working directory (.) or other writable directory in root 's executable
+#  path makes it likely that an attacker can gain superuser access by forcing an administrator
+#  operating as root to execute a Trojan horse program.
+
+class cis::cis_6_2_3 (
+  Boolean $enforced = true,
+) {
+
+  if $enforced {
+    file { 'root path integrity script':
+      ensure  => file,
+      path    => '/usr/local/bin/cis_6_2_3.sh',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0711',
+      content => file('cis/cis_6_2_3.sh'),
+    }
+    if $facts['cis_6_2_3'] != '' {
+      notify { 'cis_6_2_3':
+        message  => 'Not in compliance with CIS 6.2.3 (Scored). Integrity of root user PATH.',
+        loglevel => 'warning',
+      }
+    }
+  }
+}
