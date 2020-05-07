f57f0d1ce4b247808d0a0105abbee7b28c3a3dc0
geir <geirtufte@protonmail.com>
2020-05-07 21:12:49 +0200

    Fixing typo in which nologin. Excluding users that have /dev/null as home dir (which the default trousers tss have

--- files/cis_6_2_20.sh
+++ files/cis_6_2_20.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(whichnologin)"'" && $7 != "/bin/false") { print $1 " " $6 }' | while read -r user dir; do
+grep -E -v '^(halt|sync|shutdown)' /etc/passwd | awk -F: '($7 != "'"$(which nologin)"'" && $7 != "/bin/false" && $6 != "/dev/null") { print $1 " " $6 }' | while read -r user dir; do
   if [ ! -d "$dir" ]; then
     echo "The home directory ($dir) of user $user does not exist."
   fi
