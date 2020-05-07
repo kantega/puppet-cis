c23ce31aa8323b75651793143dc2c3e5ce178e94
geir <geirtufte@protonmail.com>
2020-05-07 12:42:51 +0200

    Exit code checking

--- lib/facter/cis_3_6.rb
+++ lib/facter/cis_3_6.rb
@@ -5,7 +5,7 @@
 Facter.add('cis_3_6') do
   setcode do
     Facter::Core::Execution.exec('grep "^GRUB_CMDLINE_LINUX.*ipv6.disable=1" /etc/default/grub')
-    if $CHILD_STATUS
+    if $?.exitstatus != 0
       true
     else
       false
