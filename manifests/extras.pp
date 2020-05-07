eaaa4845fa47044c5de5ae85ec8ff03b36f53dcd
geir <geirtufte@protonmail.com>
2020-05-07 21:06:43 +0200

    Option to remove setroubleshoot

--- manifests/extras.pp
+++ manifests/extras.pp
@@ -1,17 +1,18 @@
 # manifest disabling other irrelevant default stuff in CentOS8
 class cis::extras (
-  Boolean $enforced             = true,
-  Boolean $disable_cockpit      = true,
-  Boolean $remove_root_bin_path = true,
+  Boolean $enforced               = true,
+  Boolean $disable_cockpit        = true,
+  Boolean $remove_root_bin_path   = true,
+  Boolean $disable_setroubleshoot = true,
 ) {
   if $enforced {
     if $disable_cockpit {
-      $packages = ['cockpit-bridge',
+      $cockpit_packages = ['cockpit-bridge',
                    'cockpit-packagekit',
                    'cockpit-system',
                    'cockpit-ws',
                    'cockpit']
-      $files = [ '/etc/motd.d/cockpit',
+      $cockpit_files = [ '/etc/motd.d/cockpit',
                  '/run/cockpit/motd' ]
 
       service { 'stop and disable cockpit service':
@@ -19,10 +20,10 @@ class cis::extras (
         name   => 'cockpit',
         enable => 'false',
       }
-      package { $packages:
+      package { $cockpit_packages:
         ensure => absent,
       }
-      file { $files:
+      file { $cockpit_files:
         ensure => absent,
       }
       user { 'remove cockpit-ws user':
@@ -36,6 +37,25 @@ class cis::extras (
         path => "/root/.bash_profile",
         match => '^PATH=\$PATH:\$HOME/bin',
         match_for_absence => true
+      }
+    }
+    if $disable_setroubleshoot {
+      package { 'setroubleshoot-plugins':
+        ensure => absent,
+      }
+      file { '/etc/setroubleshoot':
+        ensure  => absent,
+        path    => '/etc/setroubleshoot',
+        force   => true.
+      }
+      file { '/var/lib/setroubleshoot':
+        ensure  => absent,
+        path    => '/var/lib/setroubleshoot',
+        force   => true.
+      }
+      user { 'remove setroubleshoot user':
+        name   => 'setroubleshoot',
+        ensure => absent,
       }
     }
   }
